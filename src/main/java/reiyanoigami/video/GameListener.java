68b48d19edf115af58a3a31cd74a3cbad316a693
Misaka <misakachan2545@gmail.com>
2021-07-10 22:27:43 +0300

    Graphics?

--- src/main/java/reiyanoigami/video/GameListener.java
+++ src/main/java/reiyanoigami/video/GameListener.java
@@ -1,13 +1,18 @@
 package reiyanoigami.video;
 
 import arc.*;
+import arc.graphics.Camera;
 import arc.graphics.g2d.*;
+import arc.util.Log;
 
 public class GameListener extends ApplicationCore {
     @Override
     public void setup() {
         Core.atlas = new TextureAtlas("sprites/sprites.aatls");
-
+        Log.info("Loaded @ sprites",Core.atlas.getRegions().size);
+        Core.batch =(Batch) new SpriteBatch();
+        Core.camera = new Camera();
+        Core.camera.position.set(Core.camera.project(0, 0));
         add(new WorldRenderer());
     }
 }
--- src/main/java/reiyanoigami/video/WorldRenderer.java
+++ src/main/java/reiyanoigami/video/WorldRenderer.java
@@ -1,25 +1,42 @@
 package reiyanoigami.video;
 
 import arc.ApplicationListener;
+import arc.Core;
 import arc.files.Fi;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.util.Log;
 import reiyanoigami.Vars;
+
+import static arc.Core.camera;
 
 public class WorldRenderer implements ApplicationListener {
     @Override
     public void init() {
+        camera.width = Core.graphics.getWidth();
+        camera.height = Core.graphics.getHeight();
     }
 
     @Override
     public void resize(int width, int height) {
-        Vars.screenW=width;
-        Vars.screenH=height;
+        camera.resize(width,height);
     }
 
     @Override
     public void update() {
         if(Vars.activeState!= Vars.GameState.gameplay){
-            return;
+            //return;
         }
+
+        camera.update();
+        Core.graphics.clear(Color.white);
+        Draw.proj(camera);
+        Draw.reset();
+
+        Draw.rect(Core.atlas.find("red"),16,16,32,32);
+
+        Draw.flush();
+        Draw.reset();
     }
 
     @Override
