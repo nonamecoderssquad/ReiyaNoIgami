ce8780984881c432ee2fe7f57feeb3feecbbe1bb
Misaka <misakachan2545@gmail.com>
2021-07-10 11:39:10 +0300

    Default window (without npe)

--- src/main/java/reiyanoigami/Main.java
+++ src/main/java/reiyanoigami/Main.java
@@ -1,9 +1,10 @@
 package reiyanoigami;
 
 import arc.backend.sdl.*;
+import reiyanoigami.video.GameListener;
 
 public class Main {
     public static void main(String[] args){
-        new SdlApplication(null,new SdlConfig());
+        new SdlApplication(new GameListener(),new SdlConfig());
     }
 }
---
+++ src/main/java/reiyanoigami/video/GameListener.java
@@ -0,0 +1,12 @@
+package reiyanoigami.video;
+
+import arc.ApplicationCore;
+import arc.files.Fi;
+
+public class GameListener extends ApplicationCore {
+
+    @Override
+    public void setup() {
+
+    }
+}
