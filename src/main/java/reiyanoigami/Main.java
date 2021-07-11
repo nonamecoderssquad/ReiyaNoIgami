1b5ff1053459edc52a0a6c2ce83ab17fde338880
shift-d <me@shift-d.tk>
2021-07-11 16:46:20 +0300

    Change window icon

--- src/main/java/reiyanoigami/Main.java
+++ src/main/java/reiyanoigami/Main.java
@@ -1,13 +1,18 @@
 package reiyanoigami;
 
 import arc.backend.sdl.*;
+import arc.Files.*;
 import reiyanoigami.video.GameListener;
 
 public class Main {
     public static void main(String[] args){
         Vars.loadLogger();
         SdlConfig gameConfig = new SdlConfig();
+        String[] filePath = new String[1];
+        filePath[0] = "sprites-raw/icon.png";
         gameConfig.title = "ReiyaNoIgami";
+        gameConfig.windowIconFileType = FileType.internal;
+        gameConfig.windowIconPaths = filePath;
         new SdlApplication(new GameListener(),gameConfig);
     }
 }
